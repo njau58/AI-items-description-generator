1f9d7611e4c4f0585f8548be8a40460517c813d1
njau58 <njauprince@gmail.com>
2023-02-01 18:07:05 +0300

    initial

--- components/hero/Hero.jsx
+++ components/hero/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <div className=' w-full bg-hero py-24 bg-no-repeat bg-center bg-cover'>
 
-      <div className='flex flex-col items-center justify-center md:mx-auto text-center max-w-2xl mx-4 '>
+      <div className='flex flex-col items-center justify-center md:mx-auto text-center max-w-2xl mx-2 '>
 
         <div className='text-5xl md:text-6xl text-white font-bold'>Generate Your Items Descriptions  
         <span className=' bg-gradient-to-r from-pink-700  to-amber-500 text-transparent bg-clip-text'> Easily</span></div>
